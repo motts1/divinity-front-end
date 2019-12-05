1af050fba8e0f3cabbb0a251d3d8a4d29baa2699
Sierra <mottsiaira@gmail.com>
2019-12-05 10:23:06 -0800

    main page update info

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,13 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Divinity</h1>
+                <p>For all your festival needs</p>
             </div>
         </div>
         <nav>
             <ul>
+           
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('Shop')}}>Shop</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('Cart')}}>Cart</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('Account')}}>Account</a></li>
